77e541444428fbc54c91b5330780bbb6c6f47037
Vinay <navik.vinay12@gmail.com>
2022-11-23 18:54:11 +0530

    UC3 Refactor 1 -find max using generic method

---
+++ GenericFindMax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsUCs
+{
+    public class GenericFindMax<T> where T: IComparable
+    {
+        public static void FindMaxValue(T first,T second,T third)  //UC3-refactor 1 (find max using generic method)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)  //data type is unknown bcoz we are using generic in the method hence error will occur so 
+            {
+                Console.WriteLine($"{first} is greater");
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{second} is greater");
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine($"{third} is greater ");
+            }
+            else
+                Console.WriteLine("All Numbers are Equal! ");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,31 @@
-﻿namespace GenericsUCs
+﻿using System.Collections;
+
+namespace GenericsUCs
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generic Use Case's ");
-            FindMaximum.FindMaxValue(20, 30, 40);                   //UC2 finding max out of 3 int.
-            FindMaximum.FindMaxValue(20.20f, 30.20f, 40.40f);       //UC2 finding max out of 3 float.
-            FindMaximum.FindMaxValue("abc", "def", "ghi");       //UC2 finding max out of 3 float.
+            Console.WriteLine("Choose any one from below options");
+            Console.WriteLine("1:Find Max Num\n" +
+                "2:FindMax using Generic Method");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FindMaximum.FindMaxValue(20, 30, 40);                   //UC1 finding max out of 3 int.
+                    FindMaximum.FindMaxValue(20.20f, 30.20f, 40.40f);       //UC2 finding max out of 3 float.
+                    FindMaximum.FindMaxValue("abc", "def", "ghi");       //UC3 finding max out of 3 string.
+                    break;
+                case 2:
+                    GenericFindMax<int>.FindMaxValue(11, 23, 30);       //find max using generic method
+                    break;
+                default:
+                    Console.WriteLine("Please choose number within given range!");
+                    break;
+            }
         }
     }
 }

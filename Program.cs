f687f7238555c9c148793bf2759053cef056b5f3
Vinay <navik.vinay12@gmail.com>
2022-11-23 17:44:09 +0530

    welcome msg in Generic UCs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsUCs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Use Case's ");
+        }
+    }
+}

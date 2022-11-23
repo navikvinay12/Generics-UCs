d89efd32b430f263f07869c1baed8ce8bc50d8b1
Vinay <navik.vinay12@gmail.com>
2022-11-23 18:03:40 +0530

    UC2 adding find max of 3 float vaues

--- FindMaximum.cs
+++ FindMaximum.cs
@@ -10,15 +10,32 @@ namespace GenericsUCs
     {
         public static void FindMaxValue(int first, int second, int third)       //UC1 finding max of 3 numbers.
         {
-            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
             {
                 Console.WriteLine($"{first} is greater");
             }
-            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
             {
                 Console.WriteLine($"{second} is greater");
             }
-            else if (third.CompareTo(first) >= 0 && third.CompareTo(second) >= 0)
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine($"{third} is greater ");
+            }
+            else
+                Console.WriteLine("All Numbers are Equal! ");
+        }
+        public static void FindMaxValue(float first, float second, float third)       //UC2 finding max out of 3 float.
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{first} is greater");
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{second} is greater");
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
             {
                 Console.WriteLine($"{third} is greater ");
             }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generic Use Case's ");
-            FindMaximum.FindMaxValue(20, 30, 40);
+            FindMaximum.FindMaxValue(20, 30, 40);                   //UC2 finding max out of 3 int.
+            FindMaximum.FindMaxValue(20.20f, 30.20f, 40.40f);       //UC2 finding max out of 3 float.
         }
     }
 }
